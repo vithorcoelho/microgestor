822bf14571e269360eb7eba58127ee692e058115
Joao <j.vithor13@gmail.com>
2019-03-10 22:38:48 -0300

    config

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,23 +1,13 @@
 <?php
 
-<<<<<<< HEAD
 	define('URLBASE', 'http://localhost/microgestor/');
-=======
-	define('URLBASE', 'https://www.microgestor.me/');
->>>>>>> master
 
 	define('URLBASE_ADMIN', (URLBASE . 'admin/'));
 
-	define('HOST', 'mysql995.umbler.com');
+	define('HOST', 'localhost');
 
 	define('DBNAME', 'erp');
 
-<<<<<<< HEAD
-	define('DBNAME2', 'system');
-
 	define('USER', 'root');
-=======
-	define('USER', 'super_admin');
->>>>>>> master
 	
-	define('PASS', 'LuciferGalileu1564');
+	define('PASS', '');
